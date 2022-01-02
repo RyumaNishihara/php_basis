bb35ca61e24aa6602830d0cd17a3416f4a3b9d84
ryuma <ryuma.nishihara@gmail.com>
2022-01-02 12:00:26 +0900

    if,matchを使った条件分岐、数字専用の入力テキスト、テキストに入力した改行を送信先でも実現する。

--- receive.php
+++ receive.php
@@ -1,3 +1,33 @@
-<?php
-  print_r($_POST);
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>出力結果</title>
+</head>
+  <body>
+    <?php
+      print_r($_POST);
+      echo '<br>';
+      // 悪意ある入力を防ぐ
+      echo htmlspecialchars($_POST['recipe_name'], ENT_QUOTES);
+      echo '<br>';
+      echo match ($_POST['category']) {
+        '1' => '和食',
+        '2' => '洋食',
+        '3' => '中華',
+      } . '<br>';
+      echo match ($_POST['difficulty']) {
+        '1' => '簡単',
+        '2' => '普通',
+        '3' => '難しい',
+      } . '<br>';
+      if (is_numeric($_POST['budget'])) {
+        echo number_format($_POST['budget']);
+      };
+      echo '<br>';
+      //改行を実現する
+      echo nl2br(htmlspecialchars($_POST['howto'], ENT_QUOTES));
+      echo '<br>';
+    ?>
+  </body>
+</html>
